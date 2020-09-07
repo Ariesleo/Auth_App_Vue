ef115fb89d92e589ffbd0510b3135e8c72ed03de
Murari <alexgupta2367@gmail.com>
2020-09-07 22:15:15 +0545

    manage user

--- src/views/Secret.vue
+++ src/views/Secret.vue
@@ -2,11 +2,26 @@
     <div>
         Secret
         <p>This is secret page</p>
+        <div class="containe">
+            <P>{{ current_user_email }}</P>
+        </div>
     </div>
 </template>
 
 <script>
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
 export default {
+    data() {
+        return {
+            current_user_email: null
+        }
+    },
+    created() {
+        const user = firebase.auth().currentUser;
+        this.current_user_email = user.email;
+    }
     
 }
 </script>
