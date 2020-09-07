919370b2e9da145a922f6cd937975272e4cdd528
Murari <alexgupta2367@gmail.com>
2020-09-07 23:08:47 +0545

    manage user

--- src/views/Secret.vue
+++ src/views/Secret.vue
@@ -3,7 +3,12 @@
         Secret
         <p>This is secret page</p>
         <div class="containe">
-            <P>{{ current_user_email }}</P>
+            <P>Email: {{ current_user_email }}</P>
+            <P>Name: {{ user_name }}</P>
+            <P>Image_URL: {{ user_photoUrl }}</P>
+            <p>Email_Verification: {{ emailVerified }}</p>
+            <p>Email_ID: {{ uid }}</p>
+            <img v-bind:src="user_photoUrl" width="100" height="100"/>
         </div>
     </div>
 </template>
@@ -15,12 +20,24 @@ import 'firebase/auth';
 export default {
     data() {
         return {
-            current_user_email: null
+            current_user_email: null,
+            user_name:null,
+            user_photoUrl: null,
+            emailVerified: null
+
         }
     },
     created() {
         const user = firebase.auth().currentUser;
-        this.current_user_email = user.email;
+        // const user_name, current_user_email, user_photoUrl;
+
+        if (user !=null) {
+            this.user_name = user.displayName;
+            this.current_user_email = user.email;
+            this.user_photoUrl = user.photoURL;
+            this.emailVerified = user.emailVerified;
+            this.uid = user.uid;
+        }
     }
     
 }
