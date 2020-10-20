7bf0f05125c76d62b6e38d96dbdc6ef6a619ef8f
Murari <alexgupta2367@gmail.com>
2020-10-20 19:06:32 +0545

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,46 +1,30 @@
+<!-- Vue Template HTML Markup -->
+
 <template>
-  <v-container class="grey lighten-5">
-    <v-row no-gutters>
-      <v-col
-        cols="12"
-        sm="4"
+<v-container>
+  <v-menu>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        v-bind="attrs"
+        v-on="on"
       >
-        <v-card
-          class="pa-2"
-          outlined
-          tile
-        >
-          One of three columns
-        </v-card>
-      </v-col>
-      <v-col
-        cols="12"
-        sm="3"
-      >
-        <v-card
-          class="pa-2"
-          outlined
-          tile
-        >
-          One of three columns
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row align="center">
-      <v-col
-        cols="12"
-        md="8"
-      >
-        <v-textarea
-          dark
-          class="black"
-          color="green"
-          outlined
-          name="input-7-4"
-          label="Outlined textarea"
-          value="The Woodman set tot the tree was soon chopped nearly through."
-        ></v-textarea>
-      </v-col>
-    </v-row>
-  </v-container>
+        Click me
+      </v-btn>
+    </template>
+
+    <v-list>
+      <v-list-item>
+        <v-list-item-title>Option 1</v-list-item-title>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-title>Option 2</v-list-item-title>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-title>Option 3</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</v-container>
 </template>
